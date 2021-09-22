f0a5ac3684bb2bad4382a36e111b7cedcdef1399
Dmitry Nezvinskyi <nezvinskyi@gmail.com>
2021-09-22 20:39:48 +0200

    refactor all components

---
+++ src/components/MainHeader/MainHeader.js
@@ -0,0 +1,7 @@
+import { Header } from 'semantic-ui-react';
+
+const MainHeader = ({ title, type = 'h1' }) => {
+  return <Header as={type}>{title}</Header>;
+};
+
+export default MainHeader;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import MainHeader from './MainHeader';
+import ButtonSaveOrCancel from './ButtonSaveOrCancel';
+import NewEntryForm from './NewEntryForm';
+import DisplayBalance from './DisplayBalance';
+import DisplayBalances from './DisplayBalances';
+import EntryLine from './EntryLine';
+
+export { MainHeader, ButtonSaveOrCancel, NewEntryForm, DisplayBalance, DisplayBalances, EntryLine };
